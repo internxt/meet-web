97def0f6c784ac05a23f1b96d3c1e0c902e37d7c
tamarafinogina <tamara@internxt.com>
2026-02-05 14:30:25 +0100

    middleware from jitsi

--- react/features/base/connection/middleware.web.ts
+++ react/features/base/connection/middleware.web.ts
@@ -1,52 +1,29 @@
-import { redirectToStaticPage } from '../../app/actions.any';
-import { CONFERENCE_WILL_LEAVE } from "../conference/actionTypes";
-import { isLeavingConferenceManually, setLeaveConferenceManually } from "../meet/general/utils/conferenceState";
-import MiddlewareRegistry from "../redux/MiddlewareRegistry";
+import MiddlewareRegistry from '../redux/MiddlewareRegistry';
 
-import { CONNECTION_DISCONNECTED, CONNECTION_WILL_CONNECT } from "./actionTypes";
+import { CONNECTION_WILL_CONNECT } from './actionTypes';
 
 /**
  * The feature announced so we can distinguish jibri participants.
  *
  * @type {string}
  */
-export const DISCO_JIBRI_FEATURE = "http://jitsi.org/protocol/jibri";
+export const DISCO_JIBRI_FEATURE = 'http://jitsi.org/protocol/jibri';
 
-MiddlewareRegistry.register(({ getState, dispatch }) => (next) => (action) => {
+MiddlewareRegistry.register(({ getState }) => next => action => {
     switch (action.type) {
-        case CONNECTION_WILL_CONNECT: {
-            const { connection } = action;
-            const { iAmRecorder } = getState()["features/base/config"];
+    case CONNECTION_WILL_CONNECT: {
+        const { connection } = action;
+        const { iAmRecorder } = getState()['features/base/config'];
 
-            if (iAmRecorder) {
-                connection.addFeature(DISCO_JIBRI_FEATURE);
-            }
-
-            // @ts-ignore
-            APP.connection = connection;
-
-            setLeaveConferenceManually(false);
-            break;
+        if (iAmRecorder) {
+            connection.addFeature(DISCO_JIBRI_FEATURE);
         }
 
-        case CONFERENCE_WILL_LEAVE: {
-            setLeaveConferenceManually(true);
-            break;
-        }
+        // @ts-ignore
+        APP.connection = connection;
 
-        case CONNECTION_DISCONNECTED: {
-            if (isLeavingConferenceManually()) {
-                setLeaveConferenceManually(false);
-
-                setTimeout(() => {
-                    dispatch(redirectToStaticPage("/"));
-                }, 2000);
-            } else {
-                console.warn("Connection disconnected unexpectedly - waiting for reconnection");
-            }
-
-            break;
-        }
+        break;
+    }
     }
 
     return next(action);
