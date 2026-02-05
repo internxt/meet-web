f3dbd36a835183b37675816f01b849e89f32d6e6
tamarafinogina <tamara@internxt.com>
2026-02-05 15:56:36 +0100

    remove style changes

--- react/features/base/connection/middleware.web.ts
+++ react/features/base/connection/middleware.web.ts
@@ -9,21 +9,21 @@ import { CONNECTION_WILL_CONNECT } from "./actionTypes";
  */
 export const DISCO_JIBRI_FEATURE = "http://jitsi.org/protocol/jibri";
 
-MiddlewareRegistry.register(({ getState }) => next => action => {
+MiddlewareRegistry.register(({ getState }) => (next) => (action) => {
     switch (action.type) {
-    case CONNECTION_WILL_CONNECT: {
+        case CONNECTION_WILL_CONNECT: {
             const { connection } = action;
             const { iAmRecorder } = getState()["features/base/config"];
 
-        if (iAmRecorder) {
-            connection.addFeature(DISCO_JIBRI_FEATURE);
+            if (iAmRecorder) {
+                connection.addFeature(DISCO_JIBRI_FEATURE);
+            }
+
+            // @ts-ignore
+            APP.connection = connection;
+
+            break;
         }
-
-        // @ts-ignore
-        APP.connection = connection;
-
-        break;
-    }
     }
 
     return next(action);
