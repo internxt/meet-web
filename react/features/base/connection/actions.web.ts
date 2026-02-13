bfc955b73b79a3fa233df34eee5b45fd18449085
tamarafinogina <tamara@internxt.com>
2026-02-13 10:07:41 +0100

    clean up conference before leaving the call

--- react/features/base/connection/actions.web.ts
+++ react/features/base/connection/actions.web.ts
@@ -128,9 +128,10 @@ export function hangup(requestFeedback = false, roomId?: string, feedbackTitle?:
 
 
 export async function cleanupAndReload(roomId: string, redirectUrl: string) {
-    console.log('[RELOAD_PAGE]: cleanup middleware');
-    await MeetingService.instance.leaveCall(roomId);
+    console.log('[RELOAD_PAGE]: Reloading the page, href:', window.location.href);
     APP.conference.cleanup();
+    console.log('[RELOAD_PAGE]: cleanup middleware, href:', window.location.href);
+    await MeetingService.instance.leaveCall(roomId);
     console.log("[RELOAD_PAGE]: Reloading the page", redirectUrl);
     window.location.replace(redirectUrl);
 }
