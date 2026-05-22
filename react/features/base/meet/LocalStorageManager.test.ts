e39cb321dbf927b33e315fdc33ca1734e60617ff
tamarafinogina <tamara@internxt.com>
2026-05-22 16:11:37 +0200

    fix tests namings

--- react/features/base/meet/LocalStorageManager.test.ts
+++ react/features/base/meet/LocalStorageManager.test.ts
@@ -10,34 +10,34 @@ describe("LocalStorageManager tests", () => {
     vi.clearAllMocks(); 
   });
 
-    it("returns the same instance on repeated access", () => {
+    it("when local storage is called repeatedly, then the same instance is returned", () => {
       const a = LocalStorageManager.instance;
       const b = LocalStorageManager.instance;
       expect(a).toBe(b);
     });
 
-    it("returns underfined when no display name is stored", () => {
+    it("when no display name is stored, then returns underfined", () => {
       expect(LocalStorageManager.instance.getDisplayName()).toBeUndefined();
     });
 
-    it("returns the stored display name when one exists", () => {
+    it("when display name is stored, then returns it", () => {
       LocalStorageManager.instance.setDisplayName(MOCK_DISPlAY_NAME);
       expect(LocalStorageManager.instance.getDisplayName()).toBe(MOCK_DISPlAY_NAME);
     });
 
-    it("returns the last stored name if was modified", () => {
+    it("when the stored display name was modified, then returns the last modification", () => {
      LocalStorageManager.instance.setDisplayName(MOCK_DISPlAY_NAME);
      const modifiedName = 'new-display-name';
      localStorage.setItem(key, modifiedName);
     expect(LocalStorageManager.instance.getDisplayName()).toBe(modifiedName);
     });
 
-    it("does not bleed into localStorage", () => {
+    it("when display name is saved in the local storage, then it does not bleed into the session storage", () => {
       LocalStorageManager.instance.setDisplayName(MOCK_DISPlAY_NAME);
       expect(sessionStorage.getItem(key)).toBeNull();
     });
 
-    it("returns underfined after local storage is clened ", () => {
+    it("when local storage is cleaned, then display name becomes underfined", () => {
       LocalStorageManager.instance.setDisplayName(MOCK_DISPlAY_NAME);
       LocalStorageManager.instance.clearCredentials();
       expect(LocalStorageManager.instance.getDisplayName()).toBeUndefined();
